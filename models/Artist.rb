4b82aeead138a9974d3ef8a19933e9c739250bc0
Gordon Bain <gbain8893@gmail.com>
2017-01-25 20:48:22 +0000

    artist and albums added and saved, but error with ID

---
+++ models/Artist.rb
@@ -0,0 +1,28 @@
+require ('pg')
+require_relative ('../db/sqlrunner')
+
+class Artist
+
+attr_reader :id, :name
+
+  def initialize(options)
+    @id = options['id'].to_i
+    @name = options['name']
+  end
+
+  def save()
+    sql = "INSERT INTO artists (name) VALUES ('#{@name}') returning *;"
+    result = SqlRunner.run(sql)
+    @id = result.first['id'].to_i
+  end
+
+  def self.all()
+    sql = "SELECT * FROM artists;"
+    artists = SqlRunner.run(sql)
+    return artists.map { |artist| Artist.new(artist)}
+  end
+
+
+
+
+end
---
+++ models/console.rb
@@ -0,0 +1,24 @@
+require('pry-byebug')
+require_relative('Album')
+require_relative('Artist')
+
+
+artist1 = Artist.new({ 'name' => 'Amy Whinehouse'})
+artist2 = Artist.new({ 'name' => 'Jon Bon Jovi'})
+
+artist1.save
+artist2.save
+
+
+@album1 = Album.new( {'title' => 'Back to Black', 'genre' => 'Blue Eyed Soul', 'artist_id' => artist1.id} )
+@album2 = Album.new({'title' => 'Frank & Back to Black', 'genre' => 'Blue Eyed Soul', 'artist_id' => artist1.id})
+@album3 = Album.new({'title' => 'Born in the USA', 'genre' => 'Rock', 'artist_id' => artist1.id})  
+@album4 = Album.new({'title' => 'Wrecking Ball', 'genre' => 'Rock', 'artist_id' => artist1.id})  
+@album5 = Album.new({'title' => 'The River', 'genre' => 'Rock', 'artist_id' => artist1.id})  
+@album6 = Album.new({'title' => 'The Rising', 'genre' => 'Rock', 'artist_id' => artist1.id})  
+
+
+binding.pry
+
+
+nil
